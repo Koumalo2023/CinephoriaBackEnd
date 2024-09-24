8a4c4e7c8e97c2fc749e6c687dac71e851d8d286
Patrice SIMO <simopatrice@yahoo.fr>
2024-09-24 22:18:43 +0200

    #388 Initialisation de la base de donnée complète necessaire pour le foncitonnement du backen

---
+++ CinephoriaBackEnd/CinephoriaBackEnd/Models/Auth/AppUsers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CinephoriaBackEnd.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string Role { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation property
+        public ICollection<Reservation> Reservations { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ CinephoriaBackEnd/CinephoriaBackEnd/Models/Incident/Incident.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinephoriaBackEnd.Models
+{
+    public class Incident
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int RoomId { get; set; }
+
+        public string Description { get; set; }
+
+        public int ReportedBy { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        // Navigation property
+        public Room Room { get; set; }
+
+        public AppUser Reporter { get; set; }
+    }
+
+}
