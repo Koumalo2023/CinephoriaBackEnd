178539ace3b06ba63e21e135fd665ca2ff45d99e
Patrice SIMO <simopatrice@yahoo.fr>
2024-09-24 20:11:44 +0200

    #380 Installer et configurer PostgreSQL

--- CinephoriaBackEnd/CinephoriaBackEnd/Program.cs
+++ CinephoriaBackEnd/CinephoriaBackEnd/Program.cs
@@ -1,7 +1,14 @@
+using CinephoriaBackEnd.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+// Ajout de CinepholiaDbContext pour la base de données PostgreSQL
+builder.Services.AddDbContext<CinephoriaDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSql")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
