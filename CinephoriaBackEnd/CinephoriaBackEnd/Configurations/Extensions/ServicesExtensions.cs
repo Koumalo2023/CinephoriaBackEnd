ef81cee1713a83761e768371b556378080868a16
Patrice SIMO <simopatrice@yahoo.fr>
2024-09-25 19:52:49 +0200

    #395 Injecter les repositories dans les services via l'injection de dépendance.

---
+++ CinephoriaBackEnd/CinephoriaBackEnd/Configurations/Extensions/ServicesExtensions.cs
@@ -0,0 +1,24 @@
+﻿using CinephoriaBackEnd.Repository;
+using Microsoft.AspNetCore.Identity;
+
+namespace CinephoriaBackEnd.Configurations
+{
+    public static class ServicesExtensions
+    {
+        public static void AddDbServiceInjection(this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor(); // Pour accéder au contexte HTTP si nécessaire
+
+            // Ajouter la gestion des utilisateurs et des connexions pour MenapUser
+            //services.AddTransient<UserManager<MenapUser>>();
+            //services.AddTransient<SignInManager<MenapUser>>();
+
+            // Injection du UoW (Unit of Work) pour Entity Framework
+            services.AddTransient<IUnitOfWorkPostgres, UnitOfWorkPostgres>();
+            services.AddTransient<IUnitOfWorkMongoDb, UnitOfWorkMongoDb>();
+
+            // Vous pouvez ajouter des services supplémentaires ici
+        }
+    }
+
+}
