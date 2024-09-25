d34d0a813be29998b63710ea300a52a10cea73d1
Patrice SIMO <simopatrice@yahoo.fr>
2024-09-25 16:36:57 +0200

    #393 Mettre en place un repository générique pour PostgreSQL.

---
+++ CinephoriaBackEnd/CinephoriaBackEnd/Repository/EntityFramwork/EFRepository.cs
@@ -0,0 +1,78 @@
+﻿
+using CinephoriaBackEnd.Data;
+using CinephoriaBackEnd.Repository.EntityFramwork;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinephoriaBackEnd.Repository
+{
+    public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly CinephoriaDbContext _efContext;
+
+        public EFRepository(CinephoriaDbContext context)
+        {
+            _efContext = context;
+        }
+
+        // Basic queries
+
+        public List<TEntity> GetAll()
+        {
+            return _efContext.Set<TEntity>().ToList();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _efContext.Set<TEntity>().ToListAsync();
+        }
+
+        public TEntity? GetById(int id)
+        {
+            return _efContext.Set<TEntity>().Find(id);
+        }
+
+        public async Task<TEntity?> GetByIdAsync(int id)
+        {
+            return await _efContext.Set<TEntity>().FindAsync(id);
+        }
+
+        // Basic commands
+
+        public void Create(TEntity entityToCreate)
+        {
+            _efContext.Set<TEntity>().Add(entityToCreate);
+            _efContext.SaveChanges(); 
+        }
+
+        public async Task CreateAsync(TEntity entityToCreate)
+        {
+            await _efContext.Set<TEntity>().AddAsync(entityToCreate);
+            await _efContext.SaveChangesAsync(); 
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+            _efContext.Set<TEntity>().Update(entityToUpdate);
+            _efContext.SaveChanges();
+        }
+
+        public async Task UpdateAsync(TEntity entityToUpdate)
+        {
+            _efContext.Set<TEntity>().Update(entityToUpdate);
+            await _efContext.SaveChangesAsync();
+        }
+
+        public void Delete(TEntity entityToDelete)
+        {
+            _efContext.Set<TEntity>().Remove(entityToDelete);
+            _efContext.SaveChanges();
+        }
+
+        public async Task DeleteAsync(TEntity entityToDelete)
+        {
+            _efContext.Set<TEntity>().Remove(entityToDelete);
+            await _efContext.SaveChangesAsync();
+        }
+    }
+
+}
