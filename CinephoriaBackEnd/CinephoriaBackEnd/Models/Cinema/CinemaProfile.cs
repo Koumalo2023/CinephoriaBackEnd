7000e66c2a69cbbf4c0aea8cd7c455d81569345f
Patrice SIMO <simopatrice@yahoo.fr>
2024-09-25 19:52:18 +0200

    #396 Mise en place des Fichiers Profile pour le mappage des tables PostgreSQL et MongoDB

---
+++ CinephoriaBackEnd/CinephoriaBackEnd/Models/Cinema/CinemaProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace CinephoriaBackEnd.Models
+{
+    public class CinemaProfile : Profile
+    {
+        public CinemaProfile()
+        {
+            CreateMap<Cinema, CinemaDto>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ CinephoriaBackEnd/CinephoriaBackEnd/Models/Review/ReviewProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace CinephoriaBackEnd.Models.Review
+{
+    using AutoMapper;
+
+    public class ReviewProfile : Profile
+    {
+        public ReviewProfile()
+        {
+            CreateMap<Review, ReviewDto>()
+                .ReverseMap();
+        }
+    }
+
+}
