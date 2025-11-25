7ba043b07d7bd3bc89041ab0d3c804dfb8ba4c92
AS-PATRICE <simopatrice@yahoo.fr>
2025-11-25 12:42:38 +0100

    feat(database): ajout de la configuration des bases de données
    - Configuration MongoDB avec paramètres de connexion et nom de base
    - Contextes de base de données pour PostgreSQL et MongoDB
    - Entité de base avec timestamps automatiques
    - Configuration des DbSets pour toutes les entités principales

---
+++ CinephoriaServer.API/Data/CinephoriaDbContext.cs
@@ -0,0 +1,81 @@
+﻿using CinephoriaServer.API.Configurations.Extensions;
+using CinephoriaServer.API.Models.MongooDb;
+using CinephoriaServer.API.Models.PostgresqlDb;
+using CinephoriaServer.API.Repository;
+using CinephoriaServer.API.Repository.Configs;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinephoriaServer.API.Data
+{
+    public class CinephoriaDbContext : IdentityDbContext<AppUser>
+    {
+        #region Common
+        public DbSet<Seat> Seats => Set<Seat>();
+        public DbSet<Movie> Movies => Set<Movie>();
+        public DbSet<Theater> Theaters => Set<Theater>();
+        public DbSet<Incident> Incidents => Set<Incident>();
+        public DbSet<Showtime> Showtimes => Set<Showtime>();
+        public DbSet<Cinema> Cinemas => Set<Cinema>();
+        public DbSet<MovieRating> MovieRatings => Set<MovieRating>();
+        public DbSet<Reservation> Reservations => Set<Reservation>();
+        public DbSet<UserMovieHistory> UserMovieHistories => Set<UserMovieHistory>();
+        public DbSet<EmployeeFavorite> EmployeeFavorites => Set<EmployeeFavorite>();
+        public DbSet<UserFavorite> UserFavorites => Set<UserFavorite>();
+        public DbSet<Setting> Settings => Set<Setting>();
+        public DbSet<MovieSimilar> MovieSimilars => Set<MovieSimilar>();
+
+        #endregion
+
+        public CinephoriaDbContext(DbContextOptions<CinephoriaDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new SeatConfig());
+            modelBuilder.ApplyConfiguration(new MovieConfig());
+            modelBuilder.ApplyConfiguration(new TheaterConfig());
+            modelBuilder.ApplyConfiguration(new IncidentConfig());
+            modelBuilder.ApplyConfiguration(new ShowtimeConfig());
+            modelBuilder.ApplyConfiguration(new CinemaConfig());
+            modelBuilder.ApplyConfiguration(new MovieRatingConfig());
+            modelBuilder.ApplyConfiguration(new ReservationConfig());
+            modelBuilder.ApplyConfiguration(new UserMovieHistoryConfig());
+            modelBuilder.ApplyConfiguration(new EmployeeFavoriteConfig());
+            modelBuilder.ApplyConfiguration(new UserFavoriteConfig());
+            modelBuilder.ApplyConfiguration(new SettingConfig());
+            modelBuilder.ApplyConfiguration(new MovieSimilarConfig());
+
+
+            modelBuilder.Ignore<AdminDashboard>();
+
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is BaseEntity && (
+                        e.State == EntityState.Added
+                        || e.State == EntityState.Modified));
+
+            foreach (var entityEntry in entries)
+            {
+                var entity = (BaseEntity)entityEntry.Entity;
+                var now = DateTime.UtcNow;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    entity.CreatedAt = now;
+                }
+
+                entity.UpdatedAt = now;
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ CinephoriaServer.API/Data/MongoDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CinephoriaServer.API.Configurations;
+using CinephoriaServer.API.Models.MongooDb;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace CinephoriaServer.API.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        // Inject MongoDbSettings instead of MongoDbContext
+        public MongoDbContext(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        // Méthode générique pour obtenir une collection
+        public IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return _database.GetCollection<T>(collectionName);
+        }
+
+        //Collections pour des types spécifiques si nécessaire
+        public IMongoCollection<AdminDashboard> AdminDashboards => _database.GetCollection<AdminDashboard>("admin_dashboard");
+       
+    }
+}
